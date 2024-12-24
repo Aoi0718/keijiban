754e51206ae35f516d4e4aac3efde107fc4b48c1
中山良太 <241025@st.yoshida-g.ac.jp>
2024-12-24 10:41:37 +0900

    abbbbbb

--- keijiban.php
+++ keijiban.php
@@ -1,25 +1,22 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>掲示板</title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        <h1>掲示板</h1>
-    <form action="name.php" method="POST">
-        <input type="submit" value="投稿者一覧">
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>登録画面</h1>
+    <form>
+        <p>ID：<input type="text" name="ID" value=""></p>
+        <p>パスワード：<input type="text" name="passwd" value=""></p>
+        <p>ユーザーネーム：<input type="text" name="uname"value=""></p>
+        
     </form>
-    <form action="insert_K.php" method="POST">
-        <input type="submit" value="記事を投稿する">
-    </form>
-<?php
-    include "../open.php";
-    $sql = "select * from toukou";
-    $sql_res = $dbh->query( $sql );
-    $html_body = "";
-    while( $rec = $sql_res->fetch() ){
-        echo "<h2>{$rec['title']}</h2>";
-        echo "<p>{$rec['date']}</p>";
-        echo "<p>{$rec['content']}</p>";
-    }
-    echo $html_body;
+    <?php
+    
+    echo <<<___EOF___
+
+    ___EOF___;
+    ?>
+</body>
+</html>
