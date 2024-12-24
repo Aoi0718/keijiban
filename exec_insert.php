c7b732be4d0d63adbbc0e0641ffd0f8d5c158355
komatsu <241014@st.yoshida-g.ac.jp>
2024-12-24 12:00:31 +0900

    変更しました

--- exec_insert.php
+++ exec_insert.php
@@ -15,18 +15,26 @@
                 // 値の取り出し
                 $title = $_POST['title'];
                 $content = $_POST['content'];
+                $login_id = $_POST['login_id'];
                 date_default_timezone_set('Asia/Tokyo');
                 $date = date("Y/m/d H:i:s");
                 // XSS対策
                 $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
                 $content = htmlspecialchars($content, ENT_QUOTES, 'UTF-8');
                 // SQL
-                $sql = "INSERT INTO toukou VALUE (null, '{$date}', '{$title}', '{$content}', null)";
+                $sql = "SELECT * FROM toukou LEFT outer join user on toukou.login_id = user.login_id";
+                $sql_res = $dbh->query( $sql );
+                $sql = "INSERT INTO toukou VALUE (null, '{$date}', '{$title}', '{$content}', '{$login_id}')";
                 $sql_res = $dbh->query( $sql );
                 
-                
-                echo "<p>投稿一覧に戻る<a href='keijiban.php'></a></p>";
+                echo "<h2>記事を追加しました。</h2>";
+                echo "<p><a href='keijiban2.php'>投稿一覧に戻る</a></p>";
             }
         ?>
+        <style>
+            h2 {
+                text-align: center;
+            }
+        </style>
     </body>
 </html>
--- keijiban2.php
+++ keijiban2.php
@@ -9,17 +9,20 @@
     <form action="name.php" method="POST">
         <input type="submit" value="投稿者一覧">
     </form>
-    <form action="insert_K.php" method="POST">
+    <form action="insert.php" method="POST">
         <input type="submit" value="記事を投稿する">
     </form>
 <?php
-    include "../open.php";
-    $sql = "select * from toukou";
+    include "../db_open.php";
+
+    $sql = "SELECT * FROM toukou ORDER BY date desc";
     $sql_res = $dbh->query( $sql );
+
     $html_body = "";
     while( $rec = $sql_res->fetch() ){
         echo "<h2>{$rec['title']}</h2>";
         echo "<p>{$rec['date']}</p>";
         echo "<p>{$rec['content']}</p>";
+        echo "<p>{$rec['login_id']}</p>";
     }
     echo $html_body;
