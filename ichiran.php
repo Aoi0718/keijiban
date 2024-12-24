94aa5b6dd6497907328e5bd9f3620d3708a65517
Aoi0718 <241004@st.yoshida-g.ac.jp>
2024-12-24 09:58:58 +0900

    ko

---
+++ ichiran.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <body>
+
+<?php
+include "../db_open.php";
+$name = $_GET[''];
+
+$sql = "select * from toukou where id = '$id'";
+$sql_res = $dbh->query( $sql );
+
+
+echo "<h2>$nameの投稿一覧</h2>";
+echo "<a href='name.php'>戻る</a>";
+while( $record = $sql_res->fetch() ){
+    echo "{$record['id']}";
+    echo "　{$record['title']}</br>";
+    echo "　　　　　　{$record['date']}</br>";
+    echo "{$record['content']}";
+}
+?>
+    </body>
+
+
+
+
+</html>
