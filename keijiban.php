166401a152402d632b83ec7ff123452bd52839cb
中山良太 <241025@st.yoshida-g.ac.jp>
2024-12-24 09:49:58 +0900

    a

--- keijiban.php
+++ keijiban.php
@@ -13,12 +13,13 @@
         <input type="submit" value="記事を投稿する">
     </form>
 <?php
-    include ".php";
+    include "../open.php";
     $sql = "select * from toukou";
     $sql_res = $dbh->query( $sql );
     $html_body = "";
     while( $rec = $sql_res->fetch() ){
         echo "<h2>{$rec['title']}</h2>";
+        echo "<p>{$rec['date']}</p>";
         echo "<p>{$rec['content']}</p>";
     }
     echo $html_body;
