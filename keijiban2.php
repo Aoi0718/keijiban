48654a29eabc6e55808a7aa4f73960fdbfefc843
中山良太 <241025@st.yoshida-g.ac.jp>
2025-01-14 11:59:22 +0900

    hennko

--- keijiban2.php
+++ keijiban2.php
@@ -3,6 +3,7 @@
     <head>
         <title>掲示板</title>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <h1>掲示板</h1>
@@ -31,6 +32,9 @@
         p{
             text-align: center;
         }
+        @media screen and (max-width: 450px){
+        }
+
     </style>
 <?php
     include "../open.php";
