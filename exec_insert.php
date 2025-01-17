0525f1f69508fc20395f5d5bc2997e9152e191a9
tikara <241020@st.yoshida-g.ac.jp>
2025-01-17 11:40:39 +0900

    a

--- exec_insert.php
+++ exec_insert.php
@@ -23,7 +23,13 @@
                 $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
                 $content = htmlspecialchars($content, ENT_QUOTES, 'UTF-8');
                 // SQL
+<<<<<<< HEAD
                 $sql = "INSERT INTO toukou VALUES (null, '{$date}', '{$title}', '{$content}', '{$login_id}')";
+=======
+                $sql = "SELECT * FROM toukou LEFT outer join user on toukou.login_id = user.login_id";
+                $sql_res = $dbh->query( $sql );
+                $sql = "INSERT INTO toukou VALUE (null, '{$date}', '{$title}', '{$content}', '{$login_id}','{$uname}')";
+>>>>>>> origin/main
                 $sql_res = $dbh->query( $sql );
                 
                 echo "<h2>記事を追加しました。</h2>";
