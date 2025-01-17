39e27915443bd0530b60e6f965853a75396df8e0
komatsu <241014@st.yoshida-g.ac.jp>
2025-01-17 10:26:02 +0900

    a

--- login_check.php
+++ login_check.php
@@ -15,8 +15,7 @@
     if($rec['login_id'] == $id && $rec['passwd'] == $passwd){
         // セッション
         $_SESSION['id'] = $id;
-        $_SESSION['login_id'] = $rec['login_id'];
-        $_SESSION['uname'] = $rec['user_name'];
+        // $_SESSION['login_id'] = $rec['login_id'];
         var_dump($_SESSION['uname']);
         
         echo "<p>ログインが完了しました。</p>";
