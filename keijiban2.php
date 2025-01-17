9c658c28b2f901b1cce540717b1fc6dfafd1f01f
komatsu <241014@st.yoshida-g.ac.jp>
2025-01-17 11:54:26 +0900

    css変更

--- keijiban2.php
+++ keijiban2.php
@@ -7,58 +7,60 @@
         <link rel="stylesheet" href="keijiban2.css">
     </head>
     <body>
-        <h1>掲示板</h1>
-    <form action="name.php" method="POST">
-        <input type="submit" value="投稿者一覧">
-    </form>
-    <form action="insert.php" method="POST">
-        <input type="submit" value="記事を投稿する">
-    </form>
-<?php
-    include "../db_open.php";
-    session_start();
+        <div class="home">
+            <h1>掲示板</h1>
+        <form action="name.php" method="POST">
+            <input type="submit" value="投稿者一覧">
+        </form>
+        <form action="insert.php" method="POST">
+            <input type="submit" value="記事を投稿する">
+        </form>
+    <?php
+        include "../db_open.php";
+        session_start();
 
-    $sql = "select * from toukou left outer join user on toukou.login_id = user.login_id order by date desc";
-    $sql_res = $dbh->query( $sql );
-    
-    echo "<h2>投稿一覧</h2>";
+        $sql = "select * from toukou left outer join user on toukou.login_id = user.login_id order by date desc";
+        $sql_res = $dbh->query( $sql );
+        
+        echo "<h2>投稿一覧</h2>";
 
-    $html_body = "";
-    while( $rec = $sql_res->fetch() ){
-        echo <<<___EOF___
-        <div class="content">
-            <div class="border">
-                <p>{$rec['id']} 【{$rec['title']}】 名前：{$rec['user_name']}　({$rec['date']})<br>{$rec['content']}
+        $html_body = "";
+        while( $rec = $sql_res->fetch() ){
+            echo <<<___EOF___
+            <div class="content">
+                <div class="border">
+                    <p>{$rec['id']} 【{$rec['title']}】 名前：{$rec['user_name']}　({$rec['date']})<br>{$rec['content']}
+                </div>
+            </div>
+
+            <style>
+                h2 {
+                    text-align: center;
+                }
+
+                .border {
+                    text-align: center;
+                }
+
+                .content {
+                    border: 1px solid #000;
+                    border-radius: 8px;
+                    margin: 16px auto;
+                    0;;
+                    list-style: none;
+                    padding: 10px 100px;
+                }
+
+                .home {
+                    margin-right: 20%;
+                    margin-left: 20%;
+                }
+            </style>
+            ___EOF___;
+        }
+        echo $html_body;
+    ?>
             </div>
         </div>
-
-        <style>
-            h2 {
-                text-align: center;
-            }
-
-            .border {
-                text-align: center;
-            }
-
-            .content {
-                border: 1px solid #000;
-                border-radius: 8px;
-                margin: 16px auto;
-                 0;;
-                list-style: none;
-                padding: 10px 100px;
-            }
-
-            .home {
-                margin-right: 20%;
-                margin-left: 20%;
-            }
-        </style>
-        ___EOF___;
-    }
-    echo $html_body;
-?>
-    </div>
     </body>
 </html>
