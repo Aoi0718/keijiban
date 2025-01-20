c1550cccb98c4bc7c1c40b74982afd43eb1477a6
tikara <241020@st.yoshida-g.ac.jp>
2025-01-20 11:24:46 +0900

    a

--- login.php
+++ login.php
@@ -16,13 +16,8 @@ session_destroy();
 
     echo "<div>";
     echo "<form method='POST' action='login_check.php'>";
-<<<<<<< HEAD
     echo "<p>       ID:<input type='text' name='id'></p>";
     echo "<p>パスワード:<input type='text' name='passwd'></p>";
-=======
-    echo "<p>　　　　ID:<input type='text' name='id' pattern='^[a-zA-Z0-9]+$+@' required></p>";
-    echo "<p>パスワード:<input type='text' name='passwd' pattern='^[a-zA-Z0-9]+$' required></p>";
->>>>>>> origin/komatsu
     echo "<a href='touroku.php'>新規登録</a>";
     echo "               ";
     echo "<input type='submit' value='ログイン'>";
