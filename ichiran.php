08f92cacc68275443dd2b61c93f472d7798e947e
komatsu <241014@st.yoshida-g.ac.jp>
2025-01-20 10:56:10 +0900

    css変更

--- ichiran.php
+++ ichiran.php
@@ -7,56 +7,59 @@
     <title>一覧画面</title>
 </head>
     <body>
-
+    <div class="home">
 <?php
-include "../db_open.php";
-$id = $_GET['id'];
-$name = $_GET['user_name'];
-$sql = "select * from toukou, user where id = '$id' && user_name = '$name'";
-$sql_res = $dbh->query( $sql );
+    include "../db_open.php";
+    // 値の取り出し
+    $id = $_GET['id'];
+    $name = $_GET['user_name'];
+    // SQL
+    $sql = "select * from toukou, user where id = '$id' && user_name = '$name'";
+    $sql_res = $dbh->query( $sql );
 
+    echo "<h2>「{$name}」の投稿一覧</h2>";
+    echo "<p><a href='name.php'>戻る</a></p>";
 
-echo "<h2>「{$name}」の投稿一覧</h2>";
-echo "<p><a href='name.php'>戻る</a></p>";
-while( $record = $sql_res->fetch() ){
-
-    echo <<<___EOF___
-        <div class="content">
-            <div class="border">
-                <p>{$record['id']} 【{$record['title']}】 ({$record['date']})<br>
-                <div class="wrap" contenteditable="true">{$record['content']}</div>
+    while( $record = $sql_res->fetch() ) {
+        echo <<<___EOF___
+            <div class="content">
+                <div class="border">
+                    <p>{$record['id']} 【{$record['title']}】 ({$record['date']})<br>
+                    <div class="wrap" contenteditable="true">{$record['content']}</div>
+                </div>
             </div>
-        </div>
 
-        <style>
-            .border {
-                    text-align: center;
-            }
+            <style>
+                .border {
+                        text-align: center;
+                }
 
-            .content {
-                border: 1px solid #000;
-                border-radius: 8px;
-                margin: 16px auto;
-                0;;
-                list-style: none;
-                padding: 10px 100px;
-            }
+                .content {
+                    border: 1px solid #000;
+                    border-radius: 8px;
+                    margin: 16px auto;
+                    0;;
+                    list-style: none;
+                    padding: 10px 100px;
+                }
 
-            .warp {
-                text-wrap: balance;
-            }
-        </style>
-    ___EOF___;
-}
+                .warp {
+                    text-wrap: balance;
+                }
+
+                .home {
+                    margin-right: 20%;
+                    margin-left: 20%;
+                }
+            </style>
+        ___EOF___;
+    }
 ?>
- <style>
+    <style>
         body {
             background-image: url("okumono_mahjonggara10-1536x864.png");
         }
     </style>
+    </div>
     </body>
-
-
-
-
 </html>
