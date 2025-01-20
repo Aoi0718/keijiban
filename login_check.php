c715c2d039f4b4f6c2e8cca5a916db013306e536
Aoi0718 <241004@st.yoshida-g.ac.jp>
2025-01-20 11:05:41 +0900

    ka

--- login_check.php
+++ login_check.php
@@ -27,7 +27,9 @@
         $_SESSION['login_id'] = $rec['login_id'];
 
         echo "<p>ログインが完了しました。</p>";
+        echo "<div>";
         echo "<a href='keijiban2.php'>掲示板へ</a>";
+        echo "</div>"; 
         exit;
     }
 
