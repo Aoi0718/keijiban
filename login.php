dbfae0cf7a22a74c1bd7599eeae8cb1bf9ba2b86
tikara <241020@st.yoshida-g.ac.jp>
2025-01-20 10:29:59 +0900

    i

--- login.php
+++ login.php
@@ -15,13 +15,8 @@ session_start();
 
     echo "<div>";
     echo "<form method='POST' action='login_check.php'>";
-<<<<<<< HEAD
     echo "<p>       ID:<input type='text' name='id'></p>";
     echo "<p>パスワード:<input type='text' name='passwd'></p>";
-=======
-    echo "<p>　　　　ID:<input type='text' name='id' pattern='^[a-zA-Z0-9]+$'></p>";
-    echo "<p>パスワード:<input type='text' name='passwd' pattern='^[a-zA-Z0-9]+$'></p>";
->>>>>>> origin/ishidaaoi
     echo "<a href='touroku.php'>新規登録</a>";
     echo "               ";
     echo "<input type='submit' value='ログイン'>";
