00f3888e2f41ce2360117a0f4d2b4d918b19f5fc
komatsu <241014@st.yoshida-g.ac.jp>
2025-01-20 09:32:45 +0900

    ichiran.php更新

--- ichiran.php
+++ ichiran.php
@@ -19,14 +19,13 @@ $sql_res = $dbh->query( $sql );
 echo "<h2>「{$name}」の投稿一覧</h2>";
 echo "<p><a href='name.php'>戻る</a></p>";
 while( $record = $sql_res->fetch() ){
+    $content = $record['content'];
+    $container = wordwrap($content,70,'<br/>',true);
 
     echo<<<___EOF___
         <div class='side'>
             <div class='center'>
-            <p>{$record['id']}</p>
-            <p>{$record['title']}</P>
-            <p>{$record['date']}</p>
-            <p>{$record['content']}</p>
+                <p>{$record['id']} 【{$record['title']}】 ({$record['date']})<br>{$container}
             </div>
         </div>
 
