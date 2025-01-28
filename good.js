85dff151562e59c8f354bc179b6a590faacaa501
Aoi0718 <241004@st.yoshida-g.ac.jp>
2025-01-28 14:18:51 +0900

    js

--- good.js
+++ good.js
@@ -1,4 +1,14 @@
-
+const mysql = require('mysql');
+  	const connection = mysql.createConnection({
+	host: 'localhost',
+    user: 'LAA1618718',
+    password: 'dbpasswd',
+	database: 'LAA1618718-mydb'                  
+    });
+    connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+    });
 document.addEventListener('DOMContentLoaded', function() {
 	var number = document.getElementsByClassName('number');
 	var likeButtons = document.getElementsByClassName('likeButton');
