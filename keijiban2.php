26f33ffb0825470b7f07a5fae4616124b0b3c59a
Aoi0718 <241004@st.yoshida-g.ac.jp>
2025-01-28 14:00:42 +0900

    いいねボタン（途中）

--- keijiban2.php
+++ keijiban2.php
@@ -44,8 +44,13 @@ if(empty($_SESSION['login_id'])){
                     <p>名前：{$rec['user_name']}</p>
                     <p>({$rec['date']})</p>
                     <div class="wrap" contenteditable="true">{$rec['content']}</div>
+                    <button type="button" class="likeButton">
+                    <svg class="likeButton__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M91.6 13A28.7 28.7 0 0 0 51 13l-1 1-1-1A28.7 28.7 0 0 0 8.4 53.8l1 1L50 95.3l40.5-40.6 1-1a28.6 28.6 0 0 0 0-40.6z"/></svg>
+                    いいね
+                    </button>
+                    <p class = number><p>
                     <form action='delete.php' method='POST'>
-                        <input type='hidden' name='id' value='{$rec['login_id']}'>
+                    <input type='hidden' name='id' value='{$rec['login_id']}'>
                     <input type='submit' value='削除'></form>
                 </div>
             </div>
@@ -54,5 +59,6 @@ if(empty($_SESSION['login_id'])){
     ?>
             </div>
         </div>
+    <script src="good.js" type="text/javascript"></script>
     </body>
 </html>
