b3eba8b70551151a90e454105e97de7a234f05f3
Aoi0718 <241004@st.yoshida-g.ac.jp>
2025-01-28 11:56:59 +0900

    ああ

---
+++ good.js
@@ -0,0 +1,6 @@
+const likeButton = document.getElementByClassName('like');
+const num = document.getElementByClassName('number');
+
+likeButton.addEventListener('click',fanction(event)){
+	number.value += 1;
+}
