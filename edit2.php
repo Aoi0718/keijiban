75e1ff5e585bce5f6ce28e093c879fa89201aed2
tikara <241020@st.yoshida-g.ac.jp>
2025-01-29 10:29:55 +0900

    a

--- edit2.php
+++ edit2.php
@@ -17,51 +17,51 @@ if(empty($_SESSION['login_id'])){
         <h2>記事の編集</h2>
 <?php
 if( $_SERVER["REQUEST_METHOD"] != "POST" ) {
-    echo "<p>不正なアクセスです。</p>";
-} else {
-    // 値の取り出し
-    $title = $_POST['title'];
-    $content = $_POST['content'];
-    date_default_timezone_set('Asia/Tokyo');
-    $date = date("Y/m/d H:i:s");
-    // セッション
-    $login_id = $_SESSION['login_id'];
-    // XSS対策
-    $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
-    $content = htmlspecialchars($content, ENT_QUOTES, 'UTF-8');
-
-    if(trim(str_replace('　','',$content)) === ''){
-    echo "スペースまたは空欄での投稿はできません";
-    echo "<p><a href='edit.php'>投稿画面に戻る</a></p>";
-    }elseif(mb_strlen( $title, "UTF-8") > 30){
-        echo "<p>タイトルは30文字以内で入力してください。<p>";
-        echo "<p><a href='edit.php'>投稿画面に戻る</a></p>";
-    }elseif(mb_strlen( $content, "UTF-8") > 200){
-        echo "<p>投稿内容は200文字以内で入力してください。<p>";
-        echo "<p><a href='edit.php'>投稿画面に戻る</a></p>";
-    } else {
-        $image = uniqid(mt_rand(), true);
-        $image .= '.' . substr(strrchr($_FILES['image']['name'], '.'), 1);
-        $file = "images/$image";
-        // SQL
-        $sql = "INSERT INTO edit VALUES (null, '{$date}', '{$title}', '{$content}', '{$login_id}', '{$image}')";
-        $sql_res = $dbh->query( $sql );
-        
-        if( !empty($_FILES['image']['name'])){
-            move_uploaded_file($_FILES['image']['tmp_name'], './images/' . $image);
-            if(exif_imagetype($file)) {
-                echo "<h2>記事を編集しました。</h2>";
-                echo "<p><a href='edit.php'>投稿一覧に戻る</a></p>";
+                echo "<p>不正なアクセスです。</p>";
             } else {
-                $message = '画像ファイルではありません。';
+                // 値の取り出し
+                $title = $_POST['title'];
+                $content = $_POST['content'];
+                date_default_timezone_set('Asia/Tokyo');
+                $date = date("Y/m/d H:i:s");
+                // セッション
+                $login_id = $_SESSION['login_id'];
+                // XSS対策
+                $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
+                $content = htmlspecialchars($content, ENT_QUOTES, 'UTF-8');
+
+                if(trim(str_replace('　','',$content)) === ''){
+                echo "スペースまたは空欄での投稿はできません";
+                echo "<p><a href='insert.php'>投稿画面に戻る</a></p>";
+                }elseif(mb_strlen( $title, "UTF-8") > 30){
+                    echo "<p>タイトルは30文字以内で入力してください。<p>";
+                    echo "<p><a href='insert.php'>投稿画面に戻る</a></p>";
+                }elseif(mb_strlen( $content, "UTF-8") > 200){
+                    echo "<p>投稿内容は200文字以内で入力してください。<p>";
+                    echo "<p><a href='insert.php'>投稿画面に戻る</a></p>";
+                } else {
+                    $image = uniqid(mt_rand(), true);
+                    $image .= '.' . substr(strrchr($_FILES['image']['name'], '.'), 1);
+                    $file = "images/$image";
+                    // SQL
+                    $sql = "INSERT INTO toukou VALUES (null, '{$date}', '{$title}', '{$content}', '{$login_id}', '{$image}')";
+                    $sql_res = $dbh->query( $sql );
+                    
+                    if( !empty($_FILES['image']['name'])){
+                        move_uploaded_file($_FILES['image']['tmp_name'], './images/' . $image);
+                        if(exif_imagetype($file)) {
+                            echo "<h2>記事を追加しました。</h2>";
+                            echo "<p><a href='keijiban2.php'>投稿一覧に戻る</a></p>";
+                        } else {
+                            $message = '画像ファイルではありません。';
+                        }
+                    }
+                }
             }
-        }
-    }
-}
-?>
+        ?>
     
     <div class="container">
-        <a href=".php" class="btn-border">戻る</a>
+        <a href="keijiban2.php" class="btn-border">戻る</a>
     </div>
     </body>
 </html>
