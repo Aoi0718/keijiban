d4df4c0fa76b5dcdf18b0cf133645b1936d50c0e
komatsu <241014@st.yoshida-g.ac.jp>
2025-02-05 11:38:17 +0900

    a

--- user_update_check.php
+++ user_update_check.php
@@ -39,16 +39,16 @@ if(empty($_SESSION['login_id'])){
                             move_uploaded_file($_FILES['icon']['tmp_name'], './images/' . $icon);
                             if(exif_imagetype($file)) {
                                 // SQL
-                                $sql = "SELECT * FROM user WHERE login_id = '{$loginID}'";
+                                // $sql = "SELECT * FROM user WHERE login_id = '{$loginID}'";
+                                // $sql_res = $dbh->query( $sql );
+                                // $rec = $sql_res->fetch();
+                                $sql = "UPDATE user SET login_id = '{$loginID}', user_name = '{$userName}', icon = '{$icon}' WHERE login_id = '{$loginID}'";
                                 $sql_res = $dbh->query( $sql );
                                 $rec = $sql_res->fetch();
-                                $sql = "UPDATE user SET login_id = '{$loginID}', user_name = '{$userName}', icon = '{$icon}' ";
-                                $sql_res = $dbh->query( $sql );
-                                $rec = $sql_res->fetch();
-                                $sql = "SELECT * FROM toukou WHERE login_id = '{$loginID}'";
-                                $sql_res = $dbh->query( $sql );
-                                $rec = $sql_res->fetch();
-                                $sql = "UPDATE toukou SET login_id = '{$loginID}'";
+                                // $sql = "SELECT * FROM toukou WHERE login_id = '{$loginID}'";
+                                // $sql_res = $dbh->query( $sql );
+                                // $rec = $sql_res->fetch();
+                                $sql = "UPDATE toukou SET login_id = '{$loginID}' WHERE login_id = '{$loginID}'";
                                 $sql_res = $dbh->query( $sql );
                                 $rec = $sql_res->fetch();
 
