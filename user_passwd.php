cccd24f8fe498c3d51acab4f92f23f192156f59d
komatsu <241014@st.yoshida-g.ac.jp>
2025-02-05 10:59:00 +0900

    パスワード変更

--- user_passwd.php
+++ user_passwd.php
@@ -16,16 +16,22 @@ if(empty($_SESSION['login_id'])){
         if( $_SERVER["REQUEST_METHOD"] != "POST" ) {
             echo "<p>不正なアクセスです。</p>";
         } else {
+            // SQL
+            $sql = "select * from toukou left outer join user on toukou.login_id = user.login_id";
+            $sql_res = $dbh->query( $sql );
+            $rec = $sql_res->fetch();
+
             echo <<<___EOF___
                 <h2>パスワード変更</h2>
                 <form method="POST" action="user_passwd_check.php">
                     <p>既存パスワード：<input type="password" name="ExPass"></p>
                     <p>新規パスワード：<input type="password" name="NewPass"></p>
                     <p>新規パスワード(確認)：<input type="password" name="NewPassCon"></p>
+                    <input type="hidden" name="login_id" value="{$rec['login_id']}">
                     <input type="submit" value="パスワードを変更">
                 </form>
                 <p><a href="user_set.php">戻る</a></p>
-            ___EOF___;    
+            ___EOF___;   
         }
     ?>
 </body>
--- user_passwd_check.php
+++ user_passwd_check.php
@@ -16,7 +16,33 @@ if(empty($_SESSION['login_id'])){
         if( $_SERVER["REQUEST_METHOD"] != "POST" ) {
             echo "<p>不正なアクセスです。</p>";
         } else {
-            
+            $loginID = $_POST["login_id"];
+            $ExPass = $_POST["ExPass"];         // 既存パスワード
+            $NewPass = $_POST["NewPass"];       // 新規パスワード 
+            $NewPassCon = $_POST["NewPassCon"]; // 新規パスワード(確認)
+            // SQL
+            $sql = "SELECT * FROM user WHERE login_id = '{$loginID}'";
+            $sql_res = $dbh->query( $sql );
+            $rec = $sql_res->fetch();
+
+            if($rec && $rec['passwd'] === $ExPass) {
+                if($NewPass === $NewPassCon) {
+                    // SQL
+                    $sql ="UPDATE user SET passwd = '{$NewPass}'";
+                    $sql_res = $dbh->query( $sql );
+                    $rec = $sql_res->fetch();
+
+                    echo "<h2>パスワードが変更されました。</h2>";
+                    echo "<p><a href='keijiban2.php'>掲示板戻る</a></p>";
+
+                } else {
+                    echo "<p>新規パスワードに誤りがあります。</p>";
+                    echo "<p><a href='user_passwd.php'>戻る</a></p>";
+                }
+            } else {
+                echo "<p>既存パスワードが間違っています。</p>";
+                echo "<p><a href='user_passwd.php'>戻る</a></p>";
+            }
         }
     ?>
 </body>
